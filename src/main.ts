6a1b992a98a76d3932987729ca921dc951f6a41b
Nerio Ramirez <nerioramirez17@gmail.com>
2023-07-06 00:27:10 -0300

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
 
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
